a5c038f7f4f5607272d66046796bc11a5e36bbcc
MohJamal <mohjamalalkhatib96@gmail.com>
2022-12-05 01:15:38 +0200

    Select

--- formik-controls-demo/src/components/FormikContainer.js
+++ formik-controls-demo/src/components/FormikContainer.js
@@ -4,14 +4,23 @@ import * as Yup from "yup";
 import FormikControl from "./FormikControl";
 
 function FormikContainer() {
+  const dropdownOptions = [
+    { key: "Select an option", value: "" },
+    { key: "Option 1", value: "option1" },
+    { key: "Option 2", value: "option2" },
+    { key: "Option 3", value: "option 3" },
+  ];
+
   const initialValues = {
     email: "",
     description: "",
+    selectOption: "",
   };
 
   const validationSchema = Yup.object({
     email: Yup.string().required("Required"),
     description: Yup.string().required("Required"),
+    selectOption: Yup.string().required("Required"),
   });
 
   const onSubmit = (values) => {
@@ -36,6 +45,12 @@ function FormikContainer() {
             control="textarea"
             label="Description"
             name="description"
+          />
+          <FormikControl
+            control="select"
+            label="Select a topic"
+            name="selectOption"
+            options={dropdownOptions}
           />
           <button type="submit">Submit</button>
         </Form>
