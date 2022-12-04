081f9ec351f50cf64193dd867ded4c4a4cfb5260
MohJamal <mohjamalalkhatib96@gmail.com>
2022-12-05 01:25:01 +0200

    Radio Buttons

--- formik-controls-demo/src/components/FormikContainer.js
+++ formik-controls-demo/src/components/FormikContainer.js
@@ -11,16 +11,24 @@ function FormikContainer() {
     { key: "Option 3", value: "option 3" },
   ];
 
+  const radioOptions = [
+    { key: "Option 1", value: "option1" },
+    { key: "Option 2", value: "option2" },
+    { key: "Option 3", value: "option 3" },
+  ];
+
   const initialValues = {
     email: "",
     description: "",
     selectOption: "",
+    radioOption: "",
   };
 
   const validationSchema = Yup.object({
     email: Yup.string().required("Required"),
     description: Yup.string().required("Required"),
     selectOption: Yup.string().required("Required"),
+    radioOption: Yup.string().required("Required"),
   });
 
   const onSubmit = (values) => {
@@ -51,6 +59,13 @@ function FormikContainer() {
             label="Select a topic"
             name="selectOption"
             options={dropdownOptions}
+          />
+
+          <FormikControl
+            control="radio"
+            label="Radio topic"
+            name="radioOption"
+            options={radioOptions}
           />
           <button type="submit">Submit</button>
         </Form>
